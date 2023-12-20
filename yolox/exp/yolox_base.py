7678dd8b668cee428f5fb902f73c5ab76effaf8a
linorman <3033797357@qq.com>
2023-12-20 13:17:40 +0800

    fix backbone

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -7,6 +7,7 @@ import random
 import torch
 import torch.distributed as dist
 import torch.nn as nn
+from yolox.models.yolo_pafpn_color import YOLOPAFPNColor
 
 from .base_exp import BaseExp
 
@@ -119,7 +120,7 @@ class Exp(BaseExp):
 
         if getattr(self, "model", None) is None:
             in_channels = [256, 512, 1024]
-            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, act=self.act)
+            backbone = YOLOPAFPNColor(self.depth, self.width, in_channels=in_channels, act=self.act)
             head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, act=self.act)
             self.model = YOLOX(backbone, head)
 
