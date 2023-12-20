a6848810c0415c722ae4ad2dd6aaae3ac1b8c184
Linorman <3033797357@qq.com>
2023-12-20 16:37:36 +0800

    change to original backbone

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -120,7 +120,7 @@ class Exp(BaseExp):
 
         if getattr(self, "model", None) is None:
             in_channels = [256, 512, 1024]
-            backbone = YOLOPAFPNColor(self.depth, self.width, in_channels=in_channels, act=self.act)
+            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, act=self.act)
             head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, act=self.act)
             self.model = YOLOX(backbone, head)
 
